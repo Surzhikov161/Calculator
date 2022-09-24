fbb3fdd61cf7a659708417d1b3c16e27959b0e67
Artem Surzhikov <surzhikov64@gmail.com>
2022-09-24 18:30:01 +0300

    removed unused variable

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
         Map<String, String> rim_arab = new HashMap<String, String>() {{
             put("I", "1");put("II", "2");put("III", "3");put("IV", "4");put("V", "5");put("VI", "6");put("VII", "7");put("VIII", "8");put("IX", "9");put("X", "10");
         }};
-        String [] rim_numbers = new String[] {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
         char action = 0;
         input = input.replaceAll("\\s+", "");
         char [] actions = {'+', '-', '/', '*'};
